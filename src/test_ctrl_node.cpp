ef632c803b8ad8d9d21bc336a82b010f2ee8cf94
ninedayhx <1170535490@qq.com>
2023-11-16 15:43:46 +0800

    refactor:从lqr纵向控制加mpc速度控制恢复到mpc纵向控制pid速度控制

--- src/test_ctrl_node.cpp
+++ src/test_ctrl_node.cpp
@@ -167,7 +167,7 @@ void controller_callback(const ros::TimerEvent &e)
 {
     static float a_tmp = 0;
 
-    LQR_longtitute.compute_ARE(car_ctrl.follow_leader_mod.A, car_ctrl.follow_leader_mod.B, true);
+    // LQR_longtitute.compute_ARE(car_ctrl.follow_leader_mod.A, car_ctrl.follow_leader_mod.B, true);
 
     // static int cnt = 0;
     // cnt++;
@@ -189,16 +189,16 @@ void controller_callback(const ros::TimerEvent &e)
     if (car_ctrl.self.start_follow)
     {
         car_ctrl.update_state_vec();
-        // mpc_lon->compute_inequality_constraints(car_ctrl.x_k.cast<double>(), (double)car_ctrl.leader.v_x, true, (double)car_ctrl.self.a_x);
+        mpc_lon->compute_inequality_constraints(car_ctrl.x_k.cast<double>(), (double)car_ctrl.leader.v_x, true, (double)car_ctrl.self.a_x);
 
-        // if (!mpc_lon->solve_MPC_QP_with_constraints(car_ctrl.x_k.cast<double>(), true))
-        // {
-        //     cout << "mpc solve fault" << endl;
-        // }
-        // ctrl_msg = car_ctrl.self.acc_to_Thr_and_Bra((float)mpc_lon->u_apply(0), mpc_filter, thr_filter);
+        if (!mpc_lon->solve_MPC_QP_with_constraints(car_ctrl.x_k.cast<double>(), true))
+        {
+            cout << "mpc solve fault" << endl;
+        }
+        ctrl_msg = car_ctrl.self.acc_to_Thr_and_Bra((float)mpc_lon->u_apply(0), mpc_filter, thr_filter);
 
         // ctrl_msg = car_ctrl.self.acc_to_Thr_and_Bra(car_ctrl.leader_follow_LQR_du_control(LQR_lon_du), true);
-        ctrl_msg = car_ctrl.self.acc_to_Thr_and_Bra(car_ctrl.leader_follow_LQR_control(LQR_longtitute), mpc_filter, thr_filter);
+        // ctrl_msg = car_ctrl.self.acc_to_Thr_and_Bra(car_ctrl.leader_follow_LQR_control(LQR_longtitute), mpc_filter, thr_filter);
 
         // cout << "following...  self lane: " << car_ctrl.lane.lane_locate << " leader lane: " << car_ctrl.leader.lane << endl;
     }
@@ -217,20 +217,20 @@ void controller_callback(const ros::TimerEvent &e)
         //     // cout << "test" << endl;
         //     car_ctrl.lon_v_des = 30;
         // }
-        // car_ctrl.lon_v_des = 30;
-        // ctrl_msg = car_ctrl.lon_speed_control(car_ctrl.lon_v_des);
+        car_ctrl.lon_v_des = 30;
+        ctrl_msg = car_ctrl.lon_speed_control(car_ctrl.lon_v_des);
         // cout << "no leader, self speed... lane = " << endl;
 
         // car_ctrl.update_state_vec();
-        car_ctrl.x_k(0) = 0;
-        car_ctrl.x_k(1) = 30 / 3.6 - car_ctrl.self.v_x;
-        car_ctrl.x_k(2) = 0;
+        // car_ctrl.x_k(0) = 0;
+        // car_ctrl.x_k(1) = 30 / 3.6 - car_ctrl.self.v_x;
+        // car_ctrl.x_k(2) = 0;
 
-        if (!mpc_sp->solve_MPC_QP_no_constraints(car_ctrl.x_k.cast<double>()))
-        {
-            cout << "mpc solve fault" << endl;
-        }
-        ctrl_msg = car_ctrl.self.acc_to_Thr_and_Bra((float)mpc_sp->u_apply(0), mpc_filter, thr_filter);
+        // if (!mpc_sp->solve_MPC_QP_no_constraints(car_ctrl.x_k.cast<double>()))
+        // {
+        //     cout << "mpc solve fault" << endl;
+        // }
+        // ctrl_msg = car_ctrl.self.acc_to_Thr_and_Bra((float)mpc_sp->u_apply(0), mpc_filter, thr_filter);
     }
 
     // cout << "self p" << car_self.lane.lane_locate << "lead p " << car_self.leader.lane << endl;
